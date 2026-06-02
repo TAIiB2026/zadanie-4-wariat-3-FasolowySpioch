0a6a4761fbbbd6cf1aa42b063738a9df276d436f
FasolowySpioch <fasolowyspioch@gmail.com>
2026-06-02 12:42:06 +0200

    pkt3

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -7,8 +7,20 @@ namespace WebAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var allowAngular = "_allowAngular";
 
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: allowAngular,
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:4106")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+                    });
+            });
+            // Add services to the container.
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -23,8 +35,9 @@ namespace WebAPI
                 app.UseSwaggerUI();
             }
 
-            app.UseAuthorization();
+            app.UseCors(allowAngular);
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
